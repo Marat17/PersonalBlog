8d1cdf2dc6fb346f10c937324b7bebcbce7780a2
Marat17 <muhutdinov@hotmail.com>
2018-10-12 17:13:12 +0300

    Main page view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from personalblog.models import Mypost
 
 # Create your views here.
+def index(request):
+	allposts = Mypost.objects.filter(time__lte=timezone.now()).order_by('-post_date')
+	context = {
+		'allposts' : allposts,
+	}
+	return render(request, 'blog/index.html', context)
