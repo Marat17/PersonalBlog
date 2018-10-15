361c44c5cc79f6c9d2152a0b3eed54a9596da333
Marat17 <muhutdinov@hotmail.com>
2018-10-15 22:49:58 +0300

    All translated

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from .models import Mypost
+from django.utils.translation import ugettext_lazy as _
 
 
 class CreateNewPost(forms.ModelForm):
-    text = forms.CharField(max_length=140, help_text='Please type your new post here:', widget=forms.Textarea)
+    text = forms.CharField(max_length=140, help_text=_('Please type your new post here:'), widget=forms.Textarea)
 
     class Meta:
         model = Mypost
