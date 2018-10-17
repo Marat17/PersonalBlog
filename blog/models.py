f2a7ad47a3363c35f8a1e6a64e3f4348d0ae3178
Marat17 <muhutdinov@hotmail.com>
2018-10-17 10:03:53 +0300

    index page is ready

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Mypost(models.Model):
     text = models.CharField(max_length=140)
     time = models.DateTimeField(default=timezone.now)
+    header = models.CharField(max_length=15, default='No')
 
     def __str__(self):
         return self.text
