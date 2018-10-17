07ed533ef00437ccb3dd1ed675f078a53245211b
Marat17 <muhutdinov@hotmail.com>
2018-10-17 14:18:51 +0300

    everything works. I only have to translate it now

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class CreateNewPost(forms.ModelForm):
+    header = forms.CharField(max_length=15, help_text=_('Title:'))
     text = forms.CharField(max_length=140, help_text=_('Please type your new post here:'), widget=forms.Textarea)
 
     class Meta:
         model = Mypost
-        fields = ('text',)
+        fields = ('header','text', )
